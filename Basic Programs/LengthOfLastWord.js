776cb683ea87983d2b455114f57e0d103406dd4a
PrajjawalBanati <prajjawalbanati16@gmail.com>
2022-11-12 14:39:52 +0530

    Added the solution of the LengthOfLastWord

--- Basic Programs/LengthOfLastWord.js
+++ Basic Programs/LengthOfLastWord.js
@@ -15,4 +15,22 @@ Explanation: The last word is "World" with length 5.
 */
 
 // *************** CODE GOES HERE ***************
+var picklastWord = function(s){
+    var lengthofstring;
+    for(i=s.length-1;i>=0;i--)
+    {
+        if (s[i]==' ')
+        {
+            lengthofstring = s.length-1 - i;
+            break;
+        }
 
+    }
+    return lengthofstring;
+}
+var readline = require("readline")
+var read = readline.createInterface(process.stdin, process.stdout)
+read.question("Enter the word of which you want to reverse the vowels ",(word)=>{
+    console.log("The length is ",picklastWord(word));
+    read.close();
+})
