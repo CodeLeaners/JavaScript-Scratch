10d441e0289cfd4ad7385640a1bfcd2e90f3034b
PrajjawalBanati <prajjawalbanati16@gmail.com>
2022-11-12 14:08:31 +0530

    Added another logic for reversevowels

--- Day1/ReverseVowels.js
+++ Day1/ReverseVowels.js
@@ -37,9 +37,36 @@ Output: "leotcede"
     //returning joined string
     return(s.join(""))
 }
+String.prototype.replaceAt = function(index,replacement){
+    return this.substring(0,index)+replacement+this.substring(index+replacement.length)
+}
+var reverseVowelsSecondSolution = function(s){
+    const vowels = ['a','e','i','o','u','A','E','I','O','U']
+    //Two pointer algorithm approach
+    for(let i=0,j=s.length;i<s.length/2;)
+    {
+        if(vowels.includes(s.charAt(i)) && vowels.includes(s.charAt(j)))
+        {
+            let temp = s[i];
+            s=s.replaceAt(i,s[j]);
+            s=s.replaceAt(j,temp);
+            i++;
+            j--;
+        }
+        else if (vowels.includes(s.charAt(i)))
+        {
+            j--;
+        }
+        else{
+            i++;
+        }
+    }
+    return s;
+}
 var readline = require("readline")
 var read = readline.createInterface(process.stdin, process.stdout)
 read.question("Enter the word of which you want to reverse the vowels ",(word)=>{
     console.log("The word is ",reverseVowels(word));
+    console.log("The word is ",reverseVowelsSecondSolution(word));
     read.close();
 })
